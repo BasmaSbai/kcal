dfcce15fd157d64980e592ab5ed197d7307aa2fa
heloufir <essbai.basma@gmail.com>
2024-02-29 14:23:22 -0500

    Premier commit

---
+++ src/test/java/com/project/kcal/KcalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
